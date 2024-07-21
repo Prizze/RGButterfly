8f638f5d052c00675252e380842a89d6fa61f60c
nar1s <pavelakuz@gmail.com>
2024-07-21 21:27:56 +0300

    Services

--- RGButterfly/web/models.py
+++ RGButterfly/web/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Service(models.Model):
+    service_name = models.CharField(max_length=255, verbose_name="Услуга")
+    service_description = models.TextField(verbose_name="Описание услуги")
+    service_image = models.ImageField(upload_to="service_images/", verbose_name="Картинка услуги")
+
+    def __str__(self):
+        return self.service_name
+
+class Option(models.Model):
+    service = models.ForeignKey(Service, related_name='options', on_delete=models.CASCADE, verbose_name="Услуга")
+    option_name = models.CharField(max_length=255, verbose_name="Опция")
+    options_description = models.TextField(verbose_name="Описание опции")
+    option_image = models.ImageField(upload_to='option_imafes/', verbose_name="Картинка опции")
+
+    def __str__(self):
+        return self.option_name
